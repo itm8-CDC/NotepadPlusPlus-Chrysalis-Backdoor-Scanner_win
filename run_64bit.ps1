b9084be54bbda689e8437c1cbff0183b690422a2
Nikolai T. Jensen <nitje@itm8.com>
2026-02-04 09:19:20 +0100

    Init conv. scanner

---
+++ run_64bit.ps1
@@ -0,0 +1,9 @@
+
+$dir = "'$PSScriptRoot'"
+If (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator))
+{
+  # Relaunch as an elevated process:
+  
+  Start-Process powershell.exe " -Command",('"set-location {0};Write-host "Running... This window will close when done"; & ".\files\velociraptor-v0.75.6-windows-amd64.exe" "artifacts --definitions=.\artifacts collect Windows.Detection.LotusBlossom.Chrysalis --output=.\chrysalis_SEE_YOUR_RESULTS_HERE.zip --format=csv --cpu_limit=5"; exit"' -f $dir) -Verb RunAs
+  exit
+}
